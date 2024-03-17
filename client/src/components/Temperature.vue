6bc262f0daf8e78496abea2d88d509376c526424
ApanLoon <apanloon@live.co.uk>
2024-03-17 20:40:34 +0100

    Styling thermometer

--- client/src/components/Temperature.vue
+++ client/src/components/Temperature.vue
@@ -12,16 +12,34 @@ if (x1Client === undefined)
 
 <template>
     <local-temperature>
-        <Thermometer :dataValue="Number(x1Client.Status.value.nozzle_temper)" :dataMin="0" :dataMax="300"></Thermometer>
+        <!-- <div>
+            <h2>Test</h2>
+            <Thermometer :currentValue="220" :targetValue="220" :valueMin="0" :valueMax="300"></Thermometer>
+        </div> -->
+        <div>
+            <h2>Nozzle</h2>
+            <Thermometer :currentValue="Number(x1Client.Status.value.nozzle_temper)" :targetValue="Number(x1Client.Status.value.nozzle_target_temper)" :valueMin="0" :valueMax="300"></Thermometer>
+        </div>
+        <div>
+            <h2>Bed</h2>
+            <Thermometer :currentValue="Number(x1Client.Status.value.bed_temper)" :targetValue="Number(x1Client.Status.value.bed_target_temper)" :valueMin="0" :valueMax="250"></Thermometer>
+        </div>
+        <div>
+            <h2>Chamber</h2>
+            <Thermometer :currentValue="Number(x1Client.Status.value.chamber_temper)" :valueMin="0" :valueMax="60"></Thermometer>
+        </div>
     </local-temperature>
 </template>
 
 <style scoped>
+h2 {text-align: center;}
+
 local-temperature
 {
     border: 1px solid var(--color-border);
     padding: 0.5rem;
     margin-top: 0.5rem;
-    display: block;
+    display: flex;
+    gap: 1rem;
 }
 </style>
--- client/src/components/generic/Thermometer.vue
+++ client/src/components/generic/Thermometer.vue
@@ -1,202 +1,130 @@
 <script setup lang="ts">
+
 const props = defineProps({
-  dataValue:  { type: Number,  required: true },
-  dataMin:    { type: Number,  required: true },
-  dataMax:    { type: Number,  required: true }
+  currentValue:  { type: Number,  required: true  },
+  targetValue:   { type: Number,  required: false, default: NaN },
+  valueMin:      { type: Number,  required: true  },
+  valueMax:      { type: Number,  required: true  }
 });
+
+const  toPercentage = (value : number) =>
+{
+    return (value - props.valueMin) / (props.valueMax - props.valueMin) * 100;
+}
+
+const isIndicatorHidden = (value : number) =>
+{
+    return Number.isNaN(value) === true;
+}
+
 </script>
 <template>
     <local-wrapper>
-        <local-meter-wrapper-outer>
-            <local-meter-wrapper-inner>
-                <meter :value="props.dataValue" :min="props.dataMin" :max="props.dataMax"></meter>
-            </local-meter-wrapper-inner>
-        </local-meter-wrapper-outer>
-        <local-temperature :data-value="props.dataValue"></local-temperature>
-        <local-gradations></local-gradations>
+        <local-thermometer>
+            <local-current :class="{hidden: isIndicatorHidden(props.currentValue)}" :style="{ '--valuenow': toPercentage(props.currentValue) + '%' }">{{ props.currentValue }}&deg;C</local-current>
+            <local-bar                                                              :style="{ '--valuenow': toPercentage(props.currentValue) + '%' }"></local-bar>
+            <local-target :class="{hidden: isIndicatorHidden(props.targetValue)}"   :style="{ '--valuenow': toPercentage(props.targetValue)  + '%' }">{{ props.targetValue  }}&deg;C</local-target>
+        </local-thermometer>
     </local-wrapper>
 </template>
 
 <style scoped>
-local-wrapper
+.hidden
 {
-    display: block;
-    /*
-    margin: auto;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-*/
-    /*
-    --TM-mainTint: #3d3d44;
-    --TM-backgroundColor: darken($TM-mainTint, 2%);
-    --TM-borderSize: 9px;
-    --TM-borderColor: darken($TM-mainTint, 8%);
-    --TM-width: 25px;
-    --TM-height: 240px;
-    --TM-bulbSize: $TM-width * 2;
-    --TM-radius: 20px;
-    --TM-graduationsStyle: 2px solid rgba(0, 0, 0, 0.5);
-    --TM-bulbColor: #3dcadf;
-    --TM-mercuryColor : linear-gradient(#f17a65, $TM-bulbColor) no-repeat bottom;
-*/
-    /* Tooltip */
-/*
-    --TM-tooltipColor: rgba(0, 0, 0, 0.7);
-    --TM-tooltipSize: 1em;
-    --TM-tooltipRadius: 5px;
-    --TM-tooltipTopShift: 5px;
-    --TM-tooltipVerticalPadding: 5px;
-    --TM-tooltipHorizontalPadding: calc(var(--TM-tooltipVerticalPadding) * 2);
-    --TM-tooltipLeftShift: 100%;
-    --TM-tooltipArrowWidth: 1.5; /* Higher numbers produce smaller width * - /
-    --TM-tooltipArrowHeight: 2.2; /* Higher numbers produce smaller height * -/
-*/
+    display: none;
 }
 
-local-meter-wrapper-outer
+local-wrapper
 {
-    display: table;
-}
-local-meter-wrapper-inner
-{
-    display: block;
-    padding: 50% 0;
-    height: 0;
-}
-meter
-{
-    display: block;
-    transform-origin: top left;
-    transform: rotate(-90deg);
-    margin-top: 50%;
+    --width:                  6rem;
+    --height:                 5rem;
+    --indicator-height:       1rem;
+    --indicator-arrow-height: 2.0;
+    --indicator-arrow-width:  1.5;
+    --bar-width:              0.5rem;
+
+    display:                  block;
+    padding-top:              calc(var(--indicator-height) * 0.5);
+    padding-bottom:           calc(var(--indicator-height) * 0.5);
 }
 
 local-thermometer
 {
-    display: block;
-
-    /*
-	width: var(--TM-width);
-	background: var(--TM-backgroundColor);
-	height: var(--TM-height);
-	position: relative;
-
-    border: var(--TM-borderSize) solid var(--TTM-borderColor);
-
-	border-radius: var(--TM-radius);
-	z-index: 1;
-	margin-bottom: var(--TM-bulbSize);
-*/
+    display:  block;
+    position: relative;
+    width:    var(--width);
+    height:   var(--height);
 }
 
-/*
-local-thermometer:before, local-thermometer:after
+local-current
 {
-    position: absolute;
-    content: "";
-    border-radius: 50%;
-}
-*/
+    position:     absolute;
+    bottom:       calc(var(--valuenow));
+    transform:    translateY(50%);
+    background:   var(--color-border);
 
-/* Border cover */
-
-/*
-local-thermometer:before
-{
-    width: 100%;
-    height: calc(var(--TM-bulbSize) / 2 + var(--TM-borderSize));
-    bottom: var(--TM-borderSize);
-    background: var(--TM-backgroundColor);
-    z-index: -1;
-}
-*/
-/* Bulb */
-/*
-local-thermometer:after
-{
-    transform: translateX(-50%);
-    width: var(--TM-bulbSize);
-    height: var(--TM-bulbSize);
-    background-color: var(--TM-bulbColor);
-    bottom: calc(-var(--TM-bulbSize) + var(--TM-borderSize));
-
-    border: var(--TM-borderSize) solid var(--TTM-borderColor);
-
-    z-index: -3;
-    left: 50%;
-}
-*/
-/*	
-local-graduations
-{
-    display: block;
-
-    height: 59%;
-    top: 20%;
-    width: 50%;
+    padding-left: 0.25rem;;
+    left:         0;
+    width:        2rem;
 }
 
-local-graduations,
-local-graduations:before
+local-target
 {
-    position: absolute;
-    border-top: var(--TM-graduationsStyle);
-    border-bottom: var(--TM-graduationsStyle);
+    position:      absolute;
+    bottom:        calc(var(--valuenow));
+    transform:     translateY(50%);
+    background:    var(--color-border);
+
+    padding-right: 0.25rem;
+    right:         0;
+    width:         2rem;
 }
 
-local-graduations:before
+local-current::after
 {
-    content: "";
-    height: 34%;
-    width: 100%;
-    top: 32%;
+    content:       "";
+    border-top:    calc(var(--indicator-height) / var(--indicator-arrow-height)) solid transparent;
+    border-bottom: calc(var(--indicator-height) / var(--indicator-arrow-height)) solid transparent;
+    border-left:   calc(var(--indicator-height) / var(--indicator-arrow-width)) solid var(--color-border);
+    position:      absolute;
+    left:          100%;
+    top:           calc(50% -  var(--indicator-height) / var(--indicator-arrow-height));
 }
 
-local-temperature
+local-target::before
 {
-    bottom: 0;
-    background: var(--TM-mercuryColor);
-    width: 100%;
-    border-radius: var(--TM-radius);
-    background-size: 100% var(--TM-height);
-    transition: all 0.2s ease-in-out;
-    
+    content:       "";
+    border-top:    calc(var(--indicator-height) / var(--indicator-arrow-height)) solid transparent;
+    border-bottom: calc(var(--indicator-height) / var(--indicator-arrow-height)) solid transparent;
+    border-right:  calc(var(--indicator-height) / var(--indicator-arrow-width)) solid var(--color-border);
+    position:      absolute;
+    right:         100%;
+    top:           calc(50% -  var(--indicator-height) / var(--indicator-arrow-height));
 }
-local-temperature,
-local-temperature:before,
-local-temperature:after
+
+local-bar
 {
-    position: absolute;
+    position:      absolute;
+    left:          calc(50% - (var(--bar-width) * 0.5));
+    width:         var(--bar-width);
+    height:        100%;
+    border:        1px solid var(--color-border);
+    border-radius: var(--bar-width);
+    background:    linear-gradient(0deg, green 0%, yellow 50%, red 100%);
 }
-*/
-/* Temperature value - Tooltip */
-/*
-local-temperature:before
+
+local-bar::after
 {
-    content: attr(data-value);
-    background: var(--TM-tooltipColor);
-    color: white;
-    z-index: 2;
-    padding: var(--TM-tooltipVerticalPadding) var(--TM-tooltipHorizontalPadding);
-    border-radius: var(--TM-tooltipRadius);
-    font-size: var(--TM-tooltipSize);
-    line-height: 1;
-    transform: translateY(50%);
-    left: calc(var(--TM-tooltipLeftShift) + 1em / var(--TM-tooltipArrowWidth));
-    top: calc(-1em + var(--TM-tooltipTopShift) - var(--TM-tooltipVerticalPadding) * 2);
+    position:                absolute;
+    left:                    0;
+    right:                   0;
+    top:                     0;
+
+    content:                 '';
+    border-top-left-radius:  var(--bar-width);
+    border-top-right-radius: var(--bar-width);
+    background:              var(--color-background);
+
+    height:                  calc(100% - var(--valuenow));
 }
-*/
-/* Tooltip arrow */
-/*
-local-temperature:after
-{
-    content: "";
-    border-top: calc(var(--TM-tooltipSize) / var(--TM-tooltipArrowHeight)) solid transparent;
-    border-bottom: calc(var(--TM-tooltipSize) / var(--TM-tooltipArrowHeight)) solid transparent;
-    border-right: calc(var(--TM-tooltipSize) / var(--TM-tooltipArrowWidth)) solid var(--TM-tooltipColor);
-    left: var(--TM-tooltipLeftShift);
-    top: calc(var(--TM-tooltipSize) / var(--TM-tooltipArrowHeight) + var(--TM-tooltipTopShift));
-}
-*/
 </style>
