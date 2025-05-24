b0fb3420548e01b027b211c6fe886a433ea5a6c8
ApanLoon <apanloon@live.co.uk>
2025-05-24 09:38:02 +0200

    hosting/config.json is now dynamic
    This makes it possible to host the application on different servers by specifying things in environment variables.
    
    Fixes #48

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -37,7 +37,8 @@ export default defineConfig (( { mode } ) =>
       (
         publicDirectories,
         {
-          ssr: false
+          ssr: false,
+          ignore: ["public/config.json"]
         }) as PluginOption
     ],
 
@@ -57,47 +58,3 @@ export default defineConfig (( { mode } ) =>
     }
   }
 });
-
-// import {defineConfig, PluginOption} from 'vite'
-// import vue from '@vitejs/plugin-vue'
-// import {DynamicPublicDirectory} from "vite-multiple-assets";
-
-// // https://vite.dev/config/
-// export default defineConfig (
-//   {
-//     server:
-//     {
-//         port: 3003
-//     },
-//     preview:
-//     {
-//         port: 3004
-//     },
-//     plugins: 
-//     [
-//       vue(
-//       {
-//         template:
-//         {
-//             transformAssetUrls:
-//             {
-//                 img: [''],
-//             }
-//         }
-//       }) as PluginOption,
-      
-//       DynamicPublicDirectory (
-//       [
-//         {
-//             input: "../../shared-assets/**",
-//             output: "/shared/images",
-//             watch: true // default
-//         },
-//         "public/**",
-//         "{\x01,public2}/**"
-//       ],
-//       {
-//         ssr: false
-//       }) as PluginOption
-//     ]
-// })
